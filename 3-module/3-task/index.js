5a75e03c85c26420830456df9aac6287746a8344
ksizova <Ksenia.Sizova@t-systems.com>
2020-11-15 11:56:12 +0300

    3-module\all-tasks completed (console.log removed)

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -6,10 +6,8 @@ function camelize(str) {
   // ваш код...
   let arr = [];
   arr = str.split('-');
-  console.log (arr);
   for (let i = 1; i < arr.length; i++) {
     arr[i] = arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1);
-    console.log(arr[i]);
   }
   let srtNew = arr.join('');
   return srtNew;
