66574337f6a4020dfa07628d504953f27908ae79
ksizova <Ksenia.Sizova@t-systems.com>
2020-11-17 11:57:40 +0300

    5-module\1,2-tasks completed

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,5 @@
 function hideSelf() {
   // ваш код...
+  const button = document.querySelector('.hide-self-button');
+  button.setAttribute('onclick', 'this.hidden=true');
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
   // ваш код...
-}
+  const button = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+  function hide() {
+    text.hidden = !text.hidden;
+  }
+  button.addEventListener('click', hide);
+ }
